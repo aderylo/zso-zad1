07e6529cc24742ef9d5a67267b4e815efffb54e3
aderylo <aderylo>
2024-03-25 18:47:41 +0000

    Refactor

--- solution/src/main.cpp
+++ solution/src/main.cpp
@@ -1,79 +1,10 @@
 #include <iostream>
-#include <elfio/elfio.hpp>
 #include <set>
 #include <map>
+#include <elfio/elfio.hpp>
+#include <utils.hpp>
 
 using namespace ELFIO;
-
-section* get_symtab_section( const ELFIO::elfio& elf_file )
-{
-    Elf_Half sec_num = elf_file.sections.size();
-    std::cout << "Number of sections: " << sec_num << std::endl;
-    for ( int i = 0; i < sec_num; ++i ) {
-        section* psec = elf_file.sections[i];
-        if ( psec->get_type() == SHT_SYMTAB ) {
-            return psec;
-        }
-    }
-
-    throw std::runtime_error( "No SYMTAB section found!" );
-}
-
-std::vector<section*> get_reloc_sections( const elfio& elf_file )
-{
-
-    Elf_Half              sec_num = elf_file.sections.size();
-    std::vector<section*> result;
-
-    for ( int i = 0; i < sec_num; ++i ) {
-        section* psec = elf_file.sections[i];
-        Elf_Word type = psec->get_type();
-        if ( type == SHT_REL || type == SHT_RELA ) {
-            result.push_back( psec );
-        }
-    }
-
-    return result;
-}
-
-struct Relocation
-{
-    Elf64_Addr offset;
-    Elf_Word   symbol_idx;
-    Elf_Word   type;
-    Elf_Sxword addend;
-
-    Relocation( Elf64_Addr offset, Elf_Word symbol_idx, Elf_Word type, Elf_Sxword addend )
-        : offset( offset ), symbol_idx( symbol_idx ), type( type ), addend( addend )
-    {
-    }
-};
-
-std::vector<Relocation> get_all_relocations( const elfio& elf_file )
-{
-    std::vector<section*>   reloc_secs = get_reloc_sections( elf_file );
-    std::vector<Relocation> result;
-
-    for ( section* reloc_sec : reloc_secs ) {
-        relocation_section_accessor relocs( elf_file, reloc_sec );
-
-        for ( uint j = 0; j < relocs.get_entries_num(); j++ ) {
-            Elf64_Addr rel_offset;
-            Elf_Word   rel_symbol_idx;
-            Elf_Word   rel_type;
-            Elf_Sxword rel_addend;
-            relocs.get_entry( j, rel_offset, rel_symbol_idx, rel_type, rel_addend );
-            result.push_back( Relocation( rel_offset, rel_symbol_idx, rel_type, rel_addend ) );
-        }
-    }
-
-    return result;
-}
-
-
-// std::vector<Symbol> get_symtab_snapshot(const elfio & elf_file){
-
-// }
 
 int main( int argc, char** argv )
 {
@@ -82,7 +13,6 @@ int main( int argc, char** argv )
         return 1;
     }
 
-    // Create an elfio reader
     elfio reader;
     elfio writer;
 
@@ -98,107 +28,46 @@ int main( int argc, char** argv )
     writer.set_type( reader.get_type() );
     writer.set_machine( reader.get_machine() );
 
-    // create empty string table
+    section* old_symtab_sec = utils::get_sections_by_type( reader, SHT_SYMTAB ).front();
     section* new_strtab_sec = writer.sections.add( ".strtab" );
-    new_strtab_sec->set_type( SHT_STRTAB );
-    new_strtab_sec->set_addr_align( 0x1 );
-
-    // create empty symbol table
-    section* old_symtab_sec = get_symtab_section( reader );
     section* new_symtab_sec = writer.sections.add( ".symtab" );
-    new_symtab_sec->set_type( old_symtab_sec->get_type() );
-    new_symtab_sec->set_entry_size( old_symtab_sec->get_entry_size() );
-    new_symtab_sec->set_flags( old_symtab_sec->get_flags() );
-    new_symtab_sec->set_addr_align( old_symtab_sec->get_addr_align() );
-    // this is ought to be changed info shall be the index of first global symbol
-    new_symtab_sec->set_info( old_symtab_sec->get_info() );
-    new_symtab_sec->set_link( new_strtab_sec->get_index() );
 
-    symbol_section_accessor symbols( reader, old_symtab_sec );
+    utils::configure_section_header( new_strtab_sec, SHT_STRTAB, 0x0, 0x0, 0x0, 0x1, 0x0 );
+    utils::configure_section_header( new_symtab_sec, SHT_SYMTAB, 0x0, 0x0,
+                                     new_strtab_sec->get_index(), old_symtab_sec->get_addr_align(),
+                                     old_symtab_sec->get_entry_size() );
+
+    symbol_section_accessor old_symbols( reader, old_symtab_sec );
     symbol_section_accessor new_symbols( writer, new_symtab_sec );
     string_section_accessor new_strings( new_strtab_sec );
 
-    std::map<int, int> old_symbol_idx_to_new; 
+    // copy symbols and create sections for them
+    for ( int idx = 0; idx < old_symbols.get_symbols_num(); idx++ ) {
+        utils::Symbol old_sym;
+        utils::get_symbol_wrapper( old_symbols, idx, old_sym );
 
-    // copy all funcitons
-    for ( unsigned int j = 0; j < symbols.get_symbols_num(); ++j ) {
-        std::string   sym_name;
-        Elf64_Addr    sym_value;
-        Elf_Xword     sym_size;
-        unsigned char sym_bind;
-        unsigned char sym_type;
-        Elf_Half      sym_section_index;
-        unsigned char sym_other;
-        symbols.get_symbol( j, sym_name, sym_value, sym_size, sym_bind, sym_type, sym_section_index,
-                            sym_other );
-
-        if ( sym_section_index == SHN_UNDEF || sym_section_index == SHN_ABS )
+        if ( old_sym.section_index == SHN_UNDEF || old_sym.section_index == SHN_ABS )
             continue;
-        // get section in which referenced object resides
-        section* psec = reader.sections[sym_section_index];
+        section* old_sec = reader.sections[old_sym.section_index];
+        section* new_sec = writer.sections.add( old_sec->get_name() + "." + old_sym.name );
 
-        // create a section for this object
-        std::string sec_name = psec->get_name() + "." + sym_name;
-        section*    f_sec    = writer.sections.add( sec_name );
-        f_sec->set_type( psec->get_type() );
-        f_sec->set_flags( psec->get_flags() );
-        f_sec->set_addr_align( psec->get_addr_align() );
+        utils::configure_section_header( new_sec, old_sec->get_type(), old_sec->get_flags(), 0x0,
+                                         0x0, old_sec->get_addr_align(),
+                                         old_sec->get_entry_size() );
 
-        if ( sym_size > 0 ) { // add instructions/data if symbol points to it
-            size_t offset_into_psec = sym_value - psec->get_address();
-            if ( offset_into_psec > psec->get_size() )
-                throw std::runtime_error( "trying to read data for fuction "
-                                          "outside of its orignal section!" );
-
-            f_sec->set_data( psec->get_data() + offset_into_psec, sym_size );
+        if ( old_sym.size > 0 ) {
+            size_t offset_into_old_sec = old_sym.value - old_sec->get_address();
+            new_sec->set_data( old_sec->get_data() + offset_into_old_sec, old_sym.size );
         }
 
-        // create relevant symbol table entry
-        Elf_Word idx = new_symbols.add_symbol( new_strings,
-                                sym_name.c_str(),  // name
-                                sym_value,         // offset into section
-                                sym_size,          // object size
-                                sym_bind,          // bind type (usualy DEFAULT)
-                                sym_type,          // type : FUNC | OBJECT
-                                sym_other,         // other
-                                f_sec->get_index() // index to a section
-        );
-        std::cout<<idx<<std::endl;
-    }
-
-    // We have necessary sections now its time to do relocations
-
-    // reverse relocations
-    std::vector<Relocation> old_relocations = get_all_relocations( reader );
-    for ( int j = 0; j < new_symbols.get_symbols_num(); j++ ) {
-        std::string   sym_name;
-        Elf64_Addr    sym_value;
-        Elf_Xword     sym_size;
-        unsigned char sym_bind;
-        unsigned char sym_type;
-        Elf_Half      sym_section_index;
-        unsigned char sym_other;
-        symbols.get_symbol( j, sym_name, sym_value, sym_size, sym_bind, sym_type, sym_section_index,
-                            sym_other );
-        section*                    sym_sec = writer.sections[sym_section_index];
-        section*                    rel_sec = writer.sections.add( ".rel" + sym_sec->get_name() );
-        relocation_section_accessor rel_sec_acc( writer, rel_sec );
-
-        // for ( auto old_rel : old_relocations ) {
-        //     if ( sym_value <= old_rel.offset && old_rel.offset <= sym_value + sym_size ) {
-        //         Elf64_Addr sec_offset = old_rel.offset - sym_value;
-
-        //         if ( old_rel.type == R_386_32 ) {
-
-
-                    
-        //             rel_sec_acc.add_entry( sec_offset,
-        //                                    old_rel.symbol_idx, // should change that
-        //                                    old_rel.type, 
-        //                                    old_rel.addend );
-        //         }
-        //     }
-        // }
+        Elf_Word sec_idx = new_symbols.add_symbol( new_strings,
+                                                   old_sym.name.c_str(), // name
+                                                   old_sym.value,        // offset into section
+                                                   old_sym.size,         // object size
+                                                   old_sym.size,  // bind type (usualy DEFAULT)
+                                                   old_sym.type,  // type : FUNC | OBJECT
+                                                   old_sym.other, // other
+                                                   new_sec->get_index() );
     }
 
     // Create additional section at the end of the file
---
+++ solution/src/utils.hpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <elfio/elfio.hpp>
+#include <set>
+#include <map>
+
+using namespace ELFIO;
+
+namespace utils {
+
+struct Relocation
+{
+    Elf64_Addr offset;
+    Elf_Word   symbol;
+    unsigned   type;
+    Elf_Sxword addend;
+};
+
+struct Symbol
+{
+    std::string   name;
+    Elf64_Addr    value;
+    Elf_Xword     size;
+    unsigned char bind;
+    unsigned char type;
+    Elf_Half      section_index;
+    unsigned char other;
+};
+
+bool get_entry_wrapper( const relocation_section_accessor& accessor,
+                        Elf_Word                           index,
+                        Relocation&                        entry )
+{
+    return accessor.get_entry( index, entry.offset, entry.symbol, entry.type, entry.addend );
+}
+
+bool get_symbol_wrapper( const symbol_section_accessor& accessor,
+                         Elf_Xword                      index,
+                         Symbol&                        symbolInfo )
+{
+    return accessor.get_symbol( index, symbolInfo.name, symbolInfo.value, symbolInfo.size,
+                                symbolInfo.bind, symbolInfo.type, symbolInfo.section_index,
+                                symbolInfo.other );
+}
+
+void configure_section_header( section*   section,
+                               Elf_Word   type,
+                               Elf_Xword  flags,
+                               Elf64_Addr addr,
+                               Elf_Word   link,
+                               Elf_Xword  addralign,
+                               Elf_Xword  entsize
+
+)
+{
+    section->set_type( type );
+    section->set_flags( flags );
+    section->set_address( addr );
+    section->set_link( link );
+    section->set_addr_align( addralign );
+    section->set_entry_size( entsize );
+}
+
+std::vector<section*> get_sections_by_type( const elfio& elf_file, Elf_Word type )
+{
+    std::vector<section*> result;
+    Elf_Half              sec_num = elf_file.sections.size();
+
+    for ( int i = 0; i < sec_num; ++i ) {
+        section* psec = elf_file.sections[i];
+        if ( type == psec->get_type() ) {
+            result.push_back( psec );
+        }
+    }
+
+    return result;
+}
+
+} // namespace utils
